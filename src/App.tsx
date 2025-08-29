ed6bdc08c1aa318a63768216206d7d8cff412dc9
pdschethana <supunichethana17@gmail.com>
2025-08-29 22:33:36 +0530

    created some parts of CourseMaterial.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { CourseMaterial } from './components/CourseMaterial';
 
 function App() {
   return (
     <div>
-      
+      <NavBar />
+      <CourseMaterial/>
     </div>
   );
 }
--- src/components/CourseMaterial.tsx
+++ src/components/CourseMaterial.tsx
@@ -1,6 +1,35 @@
+
+import Table from 'react-bootstrap/Table';
 export const CourseMaterial = () => {
     return (
         <>
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          
+          <th>Material ID</th>
+          <th>File Name</th>
+          <th>Material Type</th>
+          <th>Material</th>
+          <th>Upload At</th>
+          <th>Course ID</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          
+         
+        </tr>
+        
+      </tbody>
+    </Table>
+
         </>
     )
 }
